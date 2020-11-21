60cac10a1d24dcd9f821e4425acc8912a4e9a3ba
Nicole Casares <nicolecasares@gmail.com>
2020-11-20 21:47:59 -0800

    created validateJobTitle function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
 const nameInput = document.querySelector('input#name');
 nameInput.focus();
+
+
+const validateJobTitle = () => {
+    const titleInput = document.querySelector('#title');
+    const otherJobInput = document.querySelector('#other-job-role');
+    const otherOption = titleInput[6];
+
+    otherJobInput.style.display = "none";
+    console.log(otherOption);
+
+    if(titleInput.value == "other") {
+        alert("Please select a card type");
+        otherJobInput.style.display = "inline-block";
+    }
+
+}
+
+validateJobTitle();
+
