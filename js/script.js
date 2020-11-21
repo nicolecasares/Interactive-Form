05c81eba8bfc94b2d9c2895061c81a441f192991
Nicole Casares <nicolecasares@gmail.com>
2020-11-20 22:15:41 -0800

    added event listener

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 const nameInput = document.querySelector('input#name');
 nameInput.focus();
 
+// future nicole, wrap this in an event listener
 
+// .addEventListener('click', function(event) { 
+//     console.log(event.target.value) 
 const validateJobTitle = () => {
-    const titleInput = document.querySelector('#title').value;
+    const titleInput = document.querySelector('#title');
+    const titleInputeValue = titleInput.value;
+
     const otherJobInput = document.querySelector('#other-job-role');
+
     const otherOption = document.querySelector('option[value="other"]').value;
 
     otherJobInput.style.display = "none";
     console.log(otherOption, titleInput);
 
-    if(titleInput === otherOption) {
-        alert("Please select a card type");
-        otherJobInput.style.display = "inline-block";
-    }
+    titleInput.addEventListener('click', function(e) { 
+        console.log(e.target.value);
+
+        if(titleInput === otherOption) {
+            alert("Please select a card type");
+            otherJobInput.style.display = "inline-block";
+        }
+    }); 
+    
 
 }
 
