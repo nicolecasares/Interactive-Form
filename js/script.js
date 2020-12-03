f41f8fd949797069b9d544c542dc6bc9dd2b59c3
Nicole Casares <nicolecasares@gmail.com>
2020-12-02 21:48:37 -0800

    added validateColor function

--- js/script.js
+++ js/script.js
@@ -4,16 +4,12 @@ nameInput.focus();
 //this fuction validates the job title drop down menu. If a user selects other then they will be given the option to type one in
 const validateJobTitle = () => {
     const titleInput = document.querySelector('#title');
-    const titleInputeValue = titleInput.value;
-
     const otherJobInput = document.querySelector('#other-job-role');
-
     const otherOption = document.querySelector('option[value="other"]').value;
 
     otherJobInput.style.display = "none";
 
     titleInput.addEventListener('change', function(e) { 
-        console.log(e.target.value);
 
         if(e.target.value === 'other') {
             otherJobInput.style.display = "inline-block";
@@ -23,5 +19,34 @@ const validateJobTitle = () => {
 
 }
 
+//this fuction will make it so that the user cannot pick a color until the pick a t-shirt design
+const validateColor = () => {
+    const designLable = document.querySelector('select#design');
+    const colorLable = document.querySelector('select#color');
+
+    designLable.addEventListener('change', function(e) {
+        colorLable.removeAttribute('disabled');
+
+        //selected color options
+        const jsPunsOptions = document.querySelectorAll('#color option[data-theme = "js puns"]');
+
+        console.log(jsPunsOptions);
+        const heartJsOptions = document.querySelectorAll('#color option[data-theme = "heart js"]');  
+        
+
+        if(designLable.value === 'js puns') {
+            heartJsOptions.forEach(function(color){
+                color.style.display = 'none';
+            });
+        }else if(designLable.value === 'heart js') {
+            jsPunsOptions.forEach(function(color){
+                color.style.display = 'none';
+            });
+        }
+    });
+
+};
+
 validateJobTitle();
+validateColor();
 
